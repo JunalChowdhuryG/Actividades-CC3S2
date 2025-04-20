f8214dc48025da8bb0b62dc93c3c556e2d72807e
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-04-20 09:27:25 -0500

    Iteracion 2 Paso 1 RED: test autentificacion hash - falla

--- Actividad-09/tests/test_user_manager.py
+++ Actividad-09/tests/test_user_manager.py
@@ -1,5 +1,5 @@
 import pytest
-from src.user_manager import UserManager
+from src.user_manager import UserManager, UserNotFoundError, UserAlreadyExistsError
 
 # test agregar usuario exitoso
 def test_agregar_usuario_exitoso():
@@ -13,3 +13,32 @@ def test_agregar_usuario_exitoso():
 
     # Assert
     assert manager.user_exists(username), "El usuario debería existir después de ser agregado."
+
+
+# clase para manejar hash falsos
+class FakeHashService:
+    """
+    Servicio de hashing 'falso' (Fake) que simplemente simula el hashing
+    devolviendo la cadena con un prefijo "fakehash:" para fines de prueba.
+    """
+    def hash(self, plain_text: str) -> str:
+        return f"fakehash:{plain_text}"
+
+    def verify(self, plain_text: str, hashed_text: str) -> bool:
+        return hashed_text == f"fakehash:{plain_text}"
+
+# test agregar usuario ya existente con hash
+def test_autenticar_usuario_exitoso_con_hash():
+    # Arrange
+    hash_service = FakeHashService()
+    manager = UserManager(hash_service=hash_service)
+
+    username = "usuario1"
+    password = "mypassword123"
+    manager.add_user(username, password)
+
+    # Act
+    autenticado = manager.authenticate_user(username, password)
+
+    # Assert
+    assert autenticado, "El usuario debería autenticarse correctamente con la contraseña correcta."
