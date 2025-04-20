2ebc395aed8fd680c2feb875071cf2ad04ee718d
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-04-19 20:54:05 -0500

    ejercicio 8: agregar test parametrizado aplicar_cupon

--- Actividad-08/tests/test_cupon.py
+++ Actividad-08/tests/test_cupon.py
@@ -17,3 +17,21 @@ def test_aplicar_cupon_con_limite():
 
     # Assert
     assert total_con_cupon == 350.00
+
+
+# parametros test aplicar_cupon_parametrizado
+@pytest.mark.parametrize(
+    "precio, cantidad, porcentaje, maximo, esperado",
+    [
+        (100.0, 5, 10, 100, 450.0),  # 500 - 50
+        (100.0, 5, 50, 30, 470.0),   # 500 - 30 (limita el descuento)
+        (200.0, 1, 100, 200, 0.0),   # 200 - 200
+        (50.0, 4, 25, 20, 180.0),    # 200 - 20 (25% da 50, pero límite = 20)
+    ]
+)
+# test aplicar_cupon_parametrizado
+def test_aplicar_cupon_parametrizado(carrito, precio, cantidad, porcentaje, maximo, esperado):
+    producto = ProductoFactory(precio=precio)
+    carrito.agregar_producto(producto, cantidad)
+    total = carrito.aplicar_cupon(porcentaje, maximo)
+    assert total == esperado
