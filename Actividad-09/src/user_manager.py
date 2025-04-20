3ab12db2ab05cc8dc6d49584894ff802fd6798e2
JunalChowdhuryG <chowdhurygomezjunaljohir@gmail.com>
2025-04-20 09:27:59 -0500

    Iteracion 2 Paso 2 GREEN: implementar metodo authenticate_user

--- Actividad-09/src/user_manager.py
+++ Actividad-09/src/user_manager.py
@@ -1,14 +1,33 @@
+class UserNotFoundError(Exception):
+    pass
+
 class UserAlreadyExistsError(Exception):
     pass
 
 class UserManager:
-    def __init__(self):
+    def __init__(self, hash_service=None):
         self.users = {}
+        self.hash_service = hash_service or self._default_hash_service()
+
+    def _default_hash_service(self):
+        class DefaultHashService:
+            def hash(self, plain_text: str) -> str:
+                return plain_text
+            def verify(self, plain_text: str, hashed_text: str) -> bool:
+                return plain_text == hashed_text
+        return DefaultHashService()
 
     def add_user(self, username, password):
         if username in self.users:
             raise UserAlreadyExistsError(f"El usuario '{username}' ya existe.")
-        self.users[username] = password
+        hashed_pw = self.hash_service.hash(password)
+        self.users[username] = hashed_pw
 
     def user_exists(self, username):
         return username in self.users
+
+    def authenticate_user(self, username, password):
+        if not self.user_exists(username):
+            raise UserNotFoundError(f"El usuario '{username}' no existe.")
+        stored_hash = self.users[username]
+        return self.hash_service.verify(password, stored_hash)
